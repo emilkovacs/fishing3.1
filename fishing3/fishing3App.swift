1158fe69f6a542e4404549f4be12b75bb7a282f7
Špektrika <spektrikadev@gmail.com>
2025-07-11 11:56:41 +0200

    Initial Commit

---
+++ fishing3/fishing3App.swift
@@ -0,0 +1,17 @@
+//
+//  fishing3App.swift
+//  fishing3
+//
+//  Created by Emil Kovács on 11. 7. 2025..
+//
+
+import SwiftUI
+
+@main
+struct fishing3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
